4d6e3717484c4eaf71a2667070ce414735bc43dc
Santiago <shinca12@eafit.edu.co>
2020-01-21 11:40:42 +0100

    initial commit

---
+++ config.el
@@ -0,0 +1,11 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+(global-set-key (kbd "M-<left>")  'windmove-left)
+(global-set-key (kbd "M-<right>") 'windmove-right)
+(global-set-key (kbd "M-<up>")    'windmove-up)
+(global-set-key (kbd "M-<down>")  'windmove-down)
+
+(global-set-key (kbd "C-<left>") 'backward-word)
+(global-set-key (kbd "C-<right>") 'forward-word)
+
+(setq default-tab-width 2)
